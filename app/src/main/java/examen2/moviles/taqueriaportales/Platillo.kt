4586b8fc6d8fa1f82f4899458e3804bf785090f9
Juan Pablo Valenzuela <92190741+iTzTorero@users.noreply.github.com>
2022-03-19 13:22:08 -0700

    persistencia
    Añadi todos los platillos del menu al MainActivity.kt
    -Falta asignar las imagenes de predeterminado puse el logo.

---
+++ app/src/main/java/examen2/moviles/taqueriaportales/Platillo.kt
@@ -0,0 +1,8 @@
+package examen2.moviles.taqueriaportales
+
+data class Platillo(val id:Int,
+                    val nombre:String,
+                    val descripcion:String,
+                    val categoria:String,
+                    val costo:Float,
+                    val img:Int)
